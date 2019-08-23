c4a41de151a8b00c82eb5930f1db9fe7a0cab466
bmg <bmg>
2019-08-23 23:37:13 +0800

    提交

--- 01_Java-Introduction/src/com/bmg/Test.java
+++ 01_Java-Introduction/src/com/bmg/Test.java
@@ -1,10 +1,8 @@
 package com.bmg;
 
 public class Test {
-
 	public static void main(String[] args) {
-		System.out.println("tijiao xiangmu");
-		System.out.println("≥…π¶¡À");
+		System.out.println("tijiao");
 	}
 
 }
